9d9b072ba2bdd2a5c5a6b1f25b99c2f2bdafdd78
Victoria Fedkova <63882255+Victoria-Fedkova@users.noreply.github.com>
2023-02-26 23:16:54 +0200

    add fixes to task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,6 @@
 const list = document.querySelectorAll('#categories .item');
 console.log('Number of categories:', list.length);
 list.forEach(item => {
-  //   const category = item.firstElementChild;
   console.log('Category:', item.firstElementChild.textContent);
-  console.log('Elements:', item.querySelectorAll('li').length);
+  console.log('Elements:', item.lastElementChild.children.length);
 });
